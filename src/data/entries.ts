784f4b2a85e0d0246df7b2ff67677eb30d3df6c1
Derrick Cudd <derrickc@videoideas.com>
2019-02-25 12:25:05 -0500

    Add comments to make things more clear and helpful

--- src/data/entries.ts
+++ src/data/entries.ts
@@ -4,15 +4,21 @@ import { promisify } from 'util';
 
 import * as matter from 'gray-matter';
 
+// Currently all functions that get or modify data for entries exists in this single file.
+// This works fine for a small project such as this as it keeps it simple,
+// but for a larger project it would likely benefit from being split up
+// into different folders for queries and mutations
+
 // Promisify filesystem methods so I don't have to deal with callback hell later on
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 const exists = promisify(fs.exists);
 const writeFile = promisify(fs.writeFile);
 
-// Set the root data directory. This should be broken out eventually, but is fine here for now.
+// Set the root data directory. This should be broken out eventually, but is fine hard-coded here for now.
 const dataRoot = `${__dirname}/../../data/entries`;
 
+// Get a single entry by the slug/filename
 export const getEntry = async (slug: string) => {
   const filename = `${slug}.md`;
   const fileExists = await exists(`${dataRoot}/${filename}`);
@@ -23,6 +29,7 @@ export const getEntry = async (slug: string) => {
   }
 };
 
+// Get all entries
 export const allEntries = async () => {
   const files = await readdir(dataRoot);
 
@@ -31,6 +38,10 @@ export const allEntries = async () => {
   return entries;
 };
 
+// Create a single entry using the provides slug as the filename
+// This helps ensure unique slugs and allows us to use the slugs as an ID.
+// While this isn't super scalable and you'd likely want a unique ID for each
+// entry separate from the slug, this works well for what I want to do here.
 export const createEntry = async ({
   title,
   author,
@@ -69,6 +80,7 @@ export const createEntry = async ({
   };
 };
 
+// Update an existing entry in the filesystem with the provided data
 export const updateEntry = async ({
   title,
   author,
@@ -106,6 +118,7 @@ export const updateEntry = async ({
   }
 };
 
+// Get the parsed contents of the provided file returned as (a promise of) an object with relevant data
 const getFileContents = async (filename: string) => {
   const rawFile = await readFile(`${dataRoot}/${filename}`);
   const parsedFile = matter(rawFile);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { ApolloServer, gql } from 'apollo-server';
 
 import { allEntries, getEntry, createEntry, updateEntry } from './data/entries';
 
+// Define all of the graphql types
+// In a larger project, this should probably be broken out into its own file.
 const typeDefs = gql`
   type Entry {
     title: String!
@@ -34,16 +36,24 @@ const typeDefs = gql`
   }
 `;
 
+// Define all of the GraphQL Resolvers (How GraphQL "Resolves" certain queries and mutations)
 const resolvers = {
+  // Queries handle the fetching of data. You can typically think of these as your 'GET' requests in a typical REST API
+  // If what you want to do is just querying data and has no side effects, it should likely be a query
   Query: {
     entries: (parent, args, context, info) => allEntries(),
     entry: (parent, { slug }, context, info) => getEntry(slug),
   },
+
+  // Mutations handle, as their name suggests, the mutation of data. You can think of these as your POST/PUT/PATCH/DELETE requests in a typical REST API.
+  // Typically, if what you want to do has a side effect it should probably be a mutation
   Mutation: {
     createEntry: (parent, args, context, info) => createEntry({ ...args.data }),
     updateEntry: (parent, { slug, data }, context, info) => updateEntry({ ...data, slug }),
   },
 };
+
+// Set up the Apollo Server and run it on $PORT or 4001 if the environment variable is not defined.
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
